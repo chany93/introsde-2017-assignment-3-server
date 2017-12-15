1e68b87f5c454809a50ad47d1477fbd660fb3725
Giovanni Rafael Vuolo <grvuolo@MacBook-Pro-di-Giovanni.local>
2017-12-15 15:11:11 +0100

    final

--- src/introsde/assignment3/soap/PeopleImpl.java
+++ src/introsde/assignment3/soap/PeopleImpl.java
@@ -22,7 +22,7 @@ public class PeopleImpl implements People {
 	
 	
 	
-
+	//Method#21
     @Override
     public Person readPerson(long id) {
         System.out.println("---> Reading Person by id = "+id);
@@ -35,17 +35,21 @@ public class PeopleImpl implements People {
         return p;
     }
 
+  //Method#1
     @Override
     public List<Person> readPersonList() {
         return Person.getAll();
     }
 
+    
+  //Method#4
     @Override
     public Person createPerson(Person person) {
         Person.savePerson(person);
         return person;
     }
 
+  //Method#3
     @Override
     public Person updatePerson(Person person) {	
         Person.updatePerson(person);
@@ -53,6 +57,7 @@ public class PeopleImpl implements People {
         return person;
     }
 
+  //Method#5
     @Override
     public long deletePerson(long id) {
         Person p = Person.getPersonById(id);
@@ -64,6 +69,7 @@ public class PeopleImpl implements People {
         }
     }
     
+  //Method#6
     @Override
     public List<Activity> readPersonPreferencesByType(long id, String type) {
     	
@@ -74,11 +80,13 @@ public class PeopleImpl implements People {
 	    return personActivities;
     }
     
+  //Method#7
     @Override
     public List<Activity> readPreferences(){
     		return Activity.getAll();
     }
     
+  //Method#8
     @Override
     public Activity readPersonPreferencesById(long id, long idActivity) {
     	
@@ -93,6 +101,7 @@ public class PeopleImpl implements People {
 	    return retAct;
 }
 
+  //Method#9
 	@Override
 	public long savePersonPreferences(long id, Activity activity) {
 		
@@ -105,6 +114,7 @@ public class PeopleImpl implements People {
 		return 0;
 	}
 
+	//Method#10
 	@Override
 	public Activity updatePersonPreferences(long id, Activity activity) {
 		
@@ -118,6 +128,7 @@ public class PeopleImpl implements People {
 		return activity;
 	}
 
+	//Method#11
 	@Override
 	public Activity evaluatePersonPreferences(long id, Activity activity, int value) {
 		
@@ -126,10 +137,7 @@ public class PeopleImpl implements People {
 		
 		Map<String, Integer> eval = new HashMap<>();
 		eval = person.getEvaluation();
-		//System.out.println("it contains " + eval.containsKey(activity.getName()));
-		/*if (eval.isEmpty()) {
-			eval.put(activity, value);
-		}*/
+		
 		System.out.println("Evaluating activity with id: " + activity.getIdActivity() + " person with id: " + id);
 		
 			if (0<value && value<6 ) {
@@ -149,6 +157,8 @@ public class PeopleImpl implements People {
 		return activity;
 	}
 
+	
+	//Method#12
 	@Override
 	public List<Activity> getBestPersonPreferences(long id) {
 		
@@ -161,45 +171,6 @@ public class PeopleImpl implements People {
 		Map<String, Integer> m = new HashMap<>();
 		m = person.getEvaluation();
 		
-/*		
-		Integer max = map.entrySet()
-	            .stream()
-	            .max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1)
-	            .get()
-	            .getValue();
-
-		List listOfMax = map.entrySet()
-	            .stream()
-	            .filter(entry -> entry.getValue() == max)
-	            .map(Map.Entry::getKey)
-	            .collect(Collectors.toList());
-
-		System.out.println(listOfMax);
-	*/	
-		/*
-		Map.Entry<String, Integer> maxEntry = map.entrySet().stream()
-				  .max(Map.Entry.comparingByValue()).get();
-		
-		 for (Map.Entry<String,Integer> entry : maxEntry) {
-	    	    
- 	        System.out.println(entry.getKey() + " " + entry.getValue());
- 	    }
- 	    */
-	/*	
-		List<Map<String, Integer>> items = new ArrayList<>();
-		items.add(map);
-		for (int i = 0; i < items.size(); i++) {
-			System.out.println(++count);
-			String key = null;
-		    int value = 0;
-		    for (Map.Entry<String, Integer> entry: items.get(i).entrySet()) {
-		        if (value == (entry.getValue())) {
-		            key = (String) entry.getKey();
-		            System.out.println(key + " " + entry.getValue());
-		            break;
-		        }
-		    }
-		    */
 		
 		Integer largestVal = 0;
 		List<Entry<String, Integer>> largestLi = new ArrayList<Entry<String, Integer>>();
@@ -227,37 +198,7 @@ public class PeopleImpl implements People {
 		
 	        System.out.println(ent.getKey() + " " + ent.getValue());
 	    }
-		//System.out.println(acti.getIdActivity() + " name: " + acti.getName());
-		/*
-			for(Activity activity : activities) {
-				currentRating = map.get(activity.getName()).intValue();
-				System.out.println("for best " + bestRating + " " + activity.getName());
-				System.out.println("for current " + currentRating);
-				if (currentRating >= currentBest) {
-					currentBest = currentRating;
-					if(currentBest > bestRating) {
-						bestRating = currentBest;
-						System.out.println("hey");
-					}
-				}					
-				if (currentRating >= bestRating ) {
-					System.out.println(currentRating >= bestRating);
-					count++;
-					bestRating = currentRating;
-					System.out.println("if best " + bestRating + " " + activity.getName());
-					System.out.println("if current " + currentRating);
-					if (count == 1) {
-						currentAct = new ArrayList<>();
-					}
-					
-					currentAct.add(Activity.getActivityByNameAndPerson(activity.getName(), id));
-					
-				}else {
-					bestRating = currentBest;
-				}
-			
-		}
-		*/
+		
 		
 		return bestAct;
 	}
